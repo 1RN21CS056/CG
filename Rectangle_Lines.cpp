ee1f8774dac7033e45630c96d6ea6512ca82ef3a
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2024-05-15 12:46:20 +0530

    Add files via upload

---
+++ Rectangle_Lines.cpp
@@ -0,0 +1,36 @@
+#include<glut.h>
+
+void display()
+{
+	glClearColor(1, 1, 1, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1, 0, 0);
+	glBegin(GL_LINES);
+		glVertex2i(50, 50);
+		glVertex2i(200, 50);
+		glVertex2i(200, 50);
+		glVertex2i(200, 100);
+		glVertex2i(200, 100);
+		glVertex2i(50, 100);
+		glVertex2i(50, 100);
+		glVertex2i(50, 50);
+	glEnd();
+	glFlush();
+}
+
+void myinit()
+{
+	gluOrtho2D(0, 500, 0, 500);
+}
+
+void main()
+{
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Displaying Two Points");
+	myinit();
+	glutDisplayFunc(display);
+
+	glutMainLoop();
+}
