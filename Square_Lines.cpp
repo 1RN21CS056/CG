5a45b2d9612e35f54e3540171dd4556009984d8c
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2024-05-22 15:46:11 +0530

    Update Square_Lines.cpp

--- Square_Lines.cpp
+++ Square_Lines.cpp
@@ -28,7 +28,7 @@ void main()
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Displaying Two Points");
+	glutCreateWindow("Displaying Square Lines");
 	myinit();
 	glutDisplayFunc(display);
 
