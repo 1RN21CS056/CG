008fb7e6147891047a966ecae51a839d18a73048
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2024-06-19 12:52:25 +0530

    Update Translate_Triangle.cpp

--- Translate_Triangle.cpp
+++ Translate_Triangle.cpp
@@ -1,45 +1,50 @@
+#include<stdio.h>
 #include<glut.h>
 
-int vx1 = 50, vy1 = 50, vx2 = 150, vy2 = 50, vx3 = 100, vy3 = 100;
+float tx, ty;
 
-void myinit()
+void init()
 {
-	gluOrtho2D(0, 500, 0, 500);
+
+	gluOrtho2D(-200, 200, -200, 200);
 }
 
+void triangle(float x1, float y1, float x2, float y2, float x3, float y3)
+{
+
+	glBegin(GL_TRIANGLES);
+	glVertex2f(x1, y1);
+	glVertex2f(x2, y2);
+	glVertex2f(x3, y3);
+	glEnd();
+	glFlush();
+
+}
 void display()
 {
 	glClearColor(1, 1, 1, 1);
 	glClear(GL_COLOR_BUFFER_BIT);
-
-	glColor3f(0, 0, 0);
-	glPointSize(5);
-	glBegin(GL_TRIANGLES);
-	glVertex2i(vx1, vy1);
-	glVertex2i(vx2, vy2);
-	glVertex2i(vx3, vy3);
-	glEnd();
-
 	glColor3f(1, 0, 0);
-	glPointSize(5);
-	glBegin(GL_TRIANGLES);
-	glVertex2i(vx1 + 50, vy1 + 50);
-	glVertex2i(vx2 + 50, vy2 + 50);
-	glVertex2i(vx3 + 50, vy3 + 50);
-	glEnd();
+	triangle(-30, -30, 0, 30, 30, -30);
 
-	glFlush();
+	glColor3f(0, 1, 0);
+	triangle(-30 + tx, -30 + ty, 0 + tx, 30 + ty, 30 + tx, -30 + ty);
+
+
 }
 
 int main(int argc, char ** argv)
 {
+	printf("Enter translation values:");
+	scanf_s("%f %f", &tx, &ty);
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Rotate Triangle");
-	myinit();
+
+	glutCreateWindow("Translate Triangle");
+	init();
 	glutDisplayFunc(display);
 	glutMainLoop();
-	return 0;
 }
